1549b9bdbc96bb30cff7afcd152cabb36495ff60
Pavel Timofeev <paveltimofeev@users.noreply.github.com>
2018-02-27 19:42:10 +0300

    Add files via upload

--- checks.js
+++ checks.js
@@ -6,6 +6,7 @@ var checks = [
     { type: "DOMAIN", name: "Virustotal", url_mask: "https://www.virustotal.com/en/domain/%DOMAIN%/information/" },
     { type: "DOMAIN", name: "Observatory", url_mask: "https://observatory.mozilla.org/analyze.html?host=%DOMAIN%" },
     { type: "DOMAIN", name: "Up Guard", url_mask: "https://app.upguard.com/webscan?url=%DOMAIN%" },
+    { type: "DOMAIN", name: "Whois", url_mask: "https://www.whois.com/whois/%DOMAIN%" },
     { type: "IP", name: "Shodan", ip_mask: "https://www.shodan.io/host/%IP%" },
     { type: "*", name: "web server scanner", ip_mask: "https://pentest-tools.com/website-vulnerability-scanning/web-server-scanner" },
     { type: "*", name: "google hacking", ip_mask: "https://pentest-tools.com/information-gathering/google-hacking#" }
