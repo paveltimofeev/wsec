afec5c13da6597a33efe826104aa612c0f65e2ae
Pavel Timofeev <paveltimofeev@users.noreply.github.com>
2018-08-29 18:10:52 +0300

    Update checks.js

--- checks.js
+++ checks.js
@@ -11,6 +11,7 @@ var checks = [
     { type: "DOMAIN", name: "Security Analyser for ASP.NET", mask: "https://asafaweb.com/Scan?Url=%%" },
     { type: "DOMAIN", name: "Google Dork: Trello", mask: "https://www.google.com/search?hl=ru&q=inurl%3Atrello.com+AND+intext%3A%%" },
     { type: "IP", name: "Shodan", mask: "https://www.shodan.io/host/%%" },
+    { type: "IP", name: "I Know What You Download", mask: "https://iknowwhatyoudownload.com/ru/peer/?ip=%%" },    
     { type: "*", name: "web server scanner", mask: "https://pentest-tools.com/website-vulnerability-scanning/web-server-scanner" },
     { type: "*", name: "google hacking", mask: "https://pentest-tools.com/information-gathering/google-hacking#" }
 ];
