7c597b7610b1f422316cf6d99ff8bc0b8c27bd70
Pavel Timofeev <paveltimofeev@users.noreply.github.com>
2018-08-30 11:57:03 +0300

    Update checks.js

--- checks.js
+++ checks.js
@@ -10,9 +10,9 @@ var checks = [
     { type: "DOMAIN", name: "Whois", mask: "https://www.whois.com/whois/%%" },
     { type: "DOMAIN", name: "Security Analyser for ASP.NET", mask: "https://asafaweb.com/Scan?Url=%%" },
     { type: "DOMAIN", name: "Google Dork: Trello", mask: "https://www.google.com/search?hl=ru&q=inurl%3Atrello.com+AND+intext%3A%%" },
+    { type: "DOMAIN", name: "RusProfile (Company Name Info)", mask: "https://www.rusprofile.ru/search?query=%%" },
     { type: "IP", name: "Shodan", mask: "https://www.shodan.io/host/%%" },
     { type: "IP", name: "I Know What You Download", mask: "https://iknowwhatyoudownload.com/ru/peer/?ip=%%" },    
     { type: "*", name: "web server scanner", mask: "https://pentest-tools.com/website-vulnerability-scanning/web-server-scanner" },
-    { type: "*", name: "google hacking", mask: "https://pentest-tools.com/information-gathering/google-hacking#" },
-    { type: "DOMAIN", name: "RusProfile (Company Name Info)", mask: "https://www.rusprofile.ru/search?query=%%" }
+    { type: "*", name: "google hacking", mask: "https://pentest-tools.com/information-gathering/google-hacking#" }
 ];
